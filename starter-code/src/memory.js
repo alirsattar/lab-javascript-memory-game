49cd5110d50be9297f26e799f6f3480136e9e6d7
Ali Sattar <alirsattar@gmail.com>
2018-06-20 06:46:08 -0400

    Progress commit: MemoryGame constructor, shuffleCard, checkIfPair, and finished methods working; all but 2 potentially buggy Jasmine tests passed. 20/06/2018.

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,47 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (cards) {
+  this.cards = cards;
+  this.pickedCards = [];
+  this.pairsClicked = [];
+  this.pairsGuessed = [];
+};
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+MemoryGame.prototype.shuffleCard = function (array) {
+    var m = array.length, t, i;
+  
+    // While there remain elements to shuffle…
+    while (m) {
+  
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+  
+      // And swap it with the current element.
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+  
+    return array;
+  }
 
-// };
+MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+    this.pairsClicked++;
 
-// }
+    if (firstCard === secondCard) {
+        this.pairsGuessed++;
+        return true;
+    } else {
+        return false;
+    }
 
-// MemoryGame.prototype.finished = function () {
+}
 
-// };
+MemoryGame.prototype.finished = function () {
+if (this.pairsGuessed === 0) {
+    return false;
+} else if (this.pairsGuessed === 12){
+    return true;
+} else {
+    return false;
+}
+};
